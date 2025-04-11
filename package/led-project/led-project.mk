78cb32d809ded20482ca47b759531af2a3d9ed30
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-04-11 13:07:27 +0200

    Added proper dependencies to the package

--- package/led-project/led-project.mk
+++ package/led-project/led-project.mk
@@ -1,17 +1,17 @@
-# --- Version and Git Source ---
+# Version and Git Source
 LED_PROJECT_VERSION = main
 LED_PROJECT_SITE = https://github.com/kryczkal/RaspberryPi4LedLaboratory
 LED_PROJECT_SITE_METHOD = git
 
-# --- Standard Metadata ---
+# Standard Metadata
 LED_PROJECT_LICENSE = MIT
 LED_PROJECT_LICENSE_FILES = LICENSE # Path relative to the source root
 
-# --- Dependencies ---
-LED_PROJECT_DEPENDENCIES =
+# Dependencies
+LED_PROJECT_DEPENDENCIES = c-periphery
 
-# --- CMake Build Options ---
-LED_PROJECT_CMAKE_OPTS = -DCMAKE_BUILD_TYPE=Release \
+# CMake Build Options
+LED_PROJECT_CMAKE_OPTS = -DCMAKE_BUILD_TYPE=Debug \
 
-# --- Use the CMake package infrastructure ---
+# Use the CMake package infrastructure
 $(eval $(cmake-package))
